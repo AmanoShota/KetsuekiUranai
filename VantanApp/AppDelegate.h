221cb9500da98d28e6c06182e0593dbc014d20ac
AmanoShota <amanoshota15@gmail.com>
2014-05-12 16:12:59 +0900

    Initial Commit

---
+++ VantanApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VantanApp
+//
+//  Created by AmanoShota on 2014/05/12.
+//  Copyright (c) 2014年 ShotaAmano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
